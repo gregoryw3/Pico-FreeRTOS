08d75033e71d4942cf10c11748603f922a331fa3
gregoryw3 <greg.wainer@gmail.com>
2025-08-05 20:51:08 -0400

    Adjusted astro base path

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gregoryw3.github.io',
-  base: 'Pico-FreeRTOS/docs',
+  base: '/Pico-FreeRTOS',
   integrations: [
       starlight({
           title: 'Pico-FreeRTOS',
